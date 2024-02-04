56856ecdc6e1dceee9fd196499f25af14f8adb6c
Zeinab Abdien <suzanabdien15@gmail.com>
2024-02-04 03:57:37 +0200

    add onboarding to navigation

--- presentation/src/main/java/com/graduation/presentation/screens/auth/AuthActivity.kt
+++ presentation/src/main/java/com/graduation/presentation/screens/auth/AuthActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.graduation.core.extensions.navigation.setStartDestination
 import com.graduation.presentation.Constants.NAV_ARGS_AUTH_KEY
 import com.graduation.presentation.Constants.NAV_ARGS_AUTH_AUTHENTICATE
+import com.graduation.presentation.Constants.NAV_ARGS_AUTH_ON_BOARDING
 import com.graduation.presentation.R
 import com.graduation.presentation.databinding.ActivityAuthBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,7 +24,7 @@ class AuthActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         when (intent?.getStringExtra(NAV_ARGS_AUTH_KEY)) {
-//            NAV_ARGS_AUTH_ON_BOARDING -> handleSetStartDestination(R.id.onBoardingFragment)
+            NAV_ARGS_AUTH_ON_BOARDING -> handleSetStartDestination(R.id.onboardingFragment)
             NAV_ARGS_AUTH_AUTHENTICATE -> handleSetStartDestination(R.id.loginFragment)
             else -> handleSetStartDestination(R.id.loginFragment)
         }
