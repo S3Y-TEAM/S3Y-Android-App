33ed97f1bf55aa98d0d3e2423443fb883ed3cf7e
Zeinab Abdien <suzanabdien15@gmail.com>
2024-02-04 23:11:21 +0200

    add position fragment design

--- presentation/src/main/java/com/graduation/presentation/screens/auth/position/PositionFragment.kt
+++ presentation/src/main/java/com/graduation/presentation/screens/auth/position/PositionFragment.kt
@@ -5,16 +5,77 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.viewModels
+import com.graduation.core.base.ui.BaseViewModel
+import com.graduation.core.extensions.navigation.navigateTo
+import com.graduation.core.extensions.navigation.navigateToWithBundle
+import com.graduation.core.extensions.navigation.onBackPress
+import com.graduation.core.extensions.screen.changeStatusBarColor
 import com.graduation.presentation.R
+import com.graduation.presentation.databinding.FragmentPositionBinding
+import com.graduation.presentation.screens.BaseFragmentImpl
 
-class PositionFragment : Fragment() {
+class PositionFragment :
+    BaseFragmentImpl<FragmentPositionBinding>(FragmentPositionBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_position, container, false)
+    override val viewModel: PositionViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setOnClickListener()
+        setAppBar()
+
+    }
+
+    override fun setOnClickListener() {
+        binding.apply {
+
+            developerButton.setOnClickListener {
+                positionSelected(
+                    position = "developer"
+                )
+            }
+            skilledButton.setOnClickListener {
+                positionSelected(
+                    position = "skilled_worker"
+                )
+            }
+            userButton.setOnClickListener {
+                positionSelected(
+                    position = "user"
+                )
+            }
+            login.setOnClickListener {
+                navigateTo(R.id.action_positionFragment_to_loginFragment)
+            }
+            onBackPress {
+                requireActivity().onBackPressedDispatcher.addCallback(requireActivity()) {
+                    return@addCallback requireActivity().finish()
+                }
+            }
+        }
+    }
+
+    private fun positionSelected(position: String) {
+        navigateToWithBundle(
+            R.id.action_positionFragment_to_signUpFragment,
+            mapOf("position" to position)
+        )
+    }
+
+    override fun setAppBar() {
+        changeStatusBarColor(R.color.background, isContentLight = false, isTransparent = false)
+    }
+
+    override fun onLoadingStart() {
+    }
+
+    override fun onComplete(isSuccess: Boolean) {
+    }
+
+    override fun onCancel() {
     }
 
 }
---
+++ presentation/src/main/java/com/graduation/presentation/screens/auth/position/PositionViewModel.kt
@@ -0,0 +1,10 @@
+package com.graduation.presentation.screens.auth.position
+
+import com.graduation.core.base.ui.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+
+class PositionViewModel @Inject constructor() : BaseViewModel(){
+}
