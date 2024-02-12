22db0a136196454ff9352e9df59a9219abca33e3
Zeinab Abdien <suzanabdien15@gmail.com>
2024-02-13 00:06:13 +0200

    add certificate view model

---
+++ presentation/src/main/java/com/graduation/presentation/screens/auth/certificate/CertificateViewModel.kt
@@ -0,0 +1,9 @@
+package com.graduation.presentation.screens.auth.certificate
+
+import com.graduation.core.base.ui.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CertificateViewModel @Inject constructor() : BaseViewModel() {
+}
