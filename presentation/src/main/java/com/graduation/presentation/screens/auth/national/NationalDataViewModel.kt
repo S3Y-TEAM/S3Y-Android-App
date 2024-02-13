d24519730d09030a34a4f336b7b25158e0705c99
Zeinab Abdien <suzanabdien15@gmail.com>
2024-02-13 19:32:00 +0200

    add national view model

---
+++ presentation/src/main/java/com/graduation/presentation/screens/auth/national/NationalDataViewModel.kt
@@ -0,0 +1,7 @@
+package com.graduation.presentation.screens.auth.national
+
+import com.graduation.core.base.ui.BaseViewModel
+import javax.inject.Inject
+
+class NationalDataViewModel @Inject constructor() : BaseViewModel() {
+}
