2597d30a8b5bdc6d9efd1307c2a9307844cef29b
Zeinab Abdien <suzanabdien15@gmail.com>
2024-02-18 19:25:06 +0200

    initialize user repo fun

--- domain/src/main/java/com/graduation/domain/repositories/AuthRepository.kt
+++ domain/src/main/java/com/graduation/domain/repositories/AuthRepository.kt
@@ -1,12 +1,14 @@
 package com.graduation.domain.repositories
 
-import com.graduation.domain.models.auth.Auth.AuthRequest
-import com.graduation.domain.models.auth.Auth.AuthResponse
+import com.graduation.domain.models.auth.Auth.UserNameRequest
+import com.graduation.domain.models.auth.Auth.username.UsernameResponse
+import retrofit2.Response
 
 interface AuthRepository {
 
-//    suspend fun sendOtp(sendOtpRequest: SendOtpRequest): Response<BaseResponse>
-
-    suspend fun auth(authRequest: AuthRequest): AuthResponse
+    //    suspend fun sendOtp(sendOtpRequest: SendOtpRequest): Response<BaseResponse>
+//
+//    suspend fun auth(authRequest: AuthRequest): AuthResponse
+    suspend fun username(role: String, userNameRequest: UserNameRequest): Response<UsernameResponse>
 }
 
