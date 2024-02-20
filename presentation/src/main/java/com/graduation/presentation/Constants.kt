611b054f454d32d1a1fc8228647d73e088603a25
Zeinab Abdien <suzanabdien15@gmail.com>
2024-02-20 19:30:38 +0200

    add needed constants

--- presentation/src/main/java/com/graduation/presentation/Constants.kt
+++ presentation/src/main/java/com/graduation/presentation/Constants.kt
@@ -9,6 +9,10 @@ object Constants {
     const val NAV_ARGS_AUTH_AUTHENTICATE = "NAV_ARGS_AUTH_LOGIN"
     const val PDF_TYPE = "application/pdf/*"
     const val IMAGE_TYPE = "image/*"
+    const val DEVELOPER_KEY = "dev"
+    const val SKILLED_KEY = "worker"
+    const val USER_KEY = "emp"
+    const val VALID = "valid"
 
 
 }
