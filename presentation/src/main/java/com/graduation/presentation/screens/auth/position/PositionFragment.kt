86802a4e900644971f1efaafa554dfcefa8a91d1
Zeinab Abdien <suzanabdien15@gmail.com>
2024-02-20 19:31:19 +0200

    store role in shared preferences

--- presentation/src/main/java/com/graduation/presentation/screens/auth/position/PositionFragment.kt
+++ presentation/src/main/java/com/graduation/presentation/screens/auth/position/PositionFragment.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.graduation.core.extensions.navigation.navigateTo
-import com.graduation.core.extensions.navigation.navigateToWithBundle
 import com.graduation.core.extensions.navigation.onBackPress
 import com.graduation.core.extensions.screen.changeStatusBarColor
+import com.graduation.presentation.Constants.DEVELOPER_KEY
+import com.graduation.presentation.Constants.SKILLED_KEY
+import com.graduation.presentation.Constants.USER_KEY
 import com.graduation.presentation.R
 import com.graduation.presentation.databinding.FragmentPositionBinding
 import com.graduation.presentation.screens.BaseFragmentImpl
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class PositionFragment :
     BaseFragmentImpl<FragmentPositionBinding>(FragmentPositionBinding::inflate) {
 
     override val viewModel: PositionViewModel by viewModels()
+
+    private var isDevClicked = false
+    private var isWorkerClicked = false
+    private var isEmpClicked = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,19 +40,23 @@ class PositionFragment :
         binding.apply {
 
             developerButton.setOnClickListener {
-                positionSelected(
-                    position = "developer"
-                )
+                isDevClicked = true
+                isWorkerClicked = false
+                isEmpClicked = false
+                buttonAction()
             }
             skilledButton.setOnClickListener {
-                positionSelected(
-                    position = "skilled_worker"
-                )
+                isWorkerClicked = true
+                isDevClicked = false
+                isEmpClicked = false
+                buttonAction()
             }
             userButton.setOnClickListener {
-                positionSelected(
-                    position = "user"
-                )
+                isEmpClicked = true
+                isDevClicked = false
+                isWorkerClicked = false
+                buttonAction()
+
             }
             login.setOnClickListener {
                 navigateTo(R.id.action_positionFragment_to_loginFragment)
@@ -57,10 +70,32 @@ class PositionFragment :
     }
 
     private fun positionSelected(position: String) {
-        navigateToWithBundle(
-            R.id.action_positionFragment_to_signUpFragment,
-            mapOf("position" to position)
+        sharedPreferenceHelper.sharedRole = position
+        navigateTo(
+            R.id.action_positionFragment_to_signUpFragment
         )
+    }
+
+    private fun buttonAction() {
+        lifecycleScope.launch {
+            onLoadingStart()
+            delay(1600)
+            onComplete(true)
+            delay(500)
+
+            if (isDevClicked)
+                positionSelected(
+                    position = DEVELOPER_KEY
+                )
+            else if (isWorkerClicked)
+                positionSelected(
+                    position = SKILLED_KEY
+                )
+            else if (isEmpClicked)
+                positionSelected(
+                    position = USER_KEY
+                )
+        }
     }
 
     override fun setAppBar() {
@@ -68,9 +103,21 @@ class PositionFragment :
     }
 
     override fun onLoadingStart() {
+        if (isDevClicked)
+            binding.developerButton.start()
+        else if (isWorkerClicked)
+            binding.skilledButton.start()
+        else if (isEmpClicked)
+            binding.userButton.start()
     }
 
     override fun onComplete(isSuccess: Boolean) {
+        if (isDevClicked)
+            binding.developerButton.complete(isSuccess)
+        else if (isWorkerClicked)
+            binding.skilledButton.complete(isSuccess)
+        else if (isEmpClicked)
+            binding.userButton.complete(isSuccess)
     }
 
     override fun onCancel() {
