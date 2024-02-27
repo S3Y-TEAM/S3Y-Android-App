5be7571cb77a182ad14351c37c6bfaa7ce4fc485
Zeinab Abdien <105871085+ZeinabAbdien00@users.noreply.github.com>
2024-02-28 00:22:33 +0200

    add categories usecase

--- domain/src/main/java/com/graduation/domain/repositories/AuthRepository.kt
+++ domain/src/main/java/com/graduation/domain/repositories/AuthRepository.kt
@@ -1,6 +1,7 @@
 package com.graduation.domain.repositories
 
 import com.graduation.domain.models.auth.Auth.UserNameRequest
+import com.graduation.domain.models.auth.Auth.categories.CategoriesResponse
 import com.graduation.domain.models.auth.Auth.username.UsernameResponse
 import retrofit2.Response
 
@@ -10,5 +11,6 @@ interface AuthRepository {
 //
 //    suspend fun auth(authRequest: AuthRequest): AuthResponse
     suspend fun username(role: String, userNameRequest: UserNameRequest): Response<UsernameResponse>
+    suspend fun categories(role: String, categoriesRequest: UserNameRequest): Response<CategoriesResponse>
 }
 
---
+++ domain/src/main/java/com/graduation/domain/usecase/auth/CategoriesUseCase.kt
@@ -0,0 +1,19 @@
+package com.graduation.domain.usecase.auth
+
+import com.graduation.domain.models.auth.Auth.UserNameRequest
+import com.graduation.domain.models.auth.Auth.categories.CategoriesResponse
+import com.graduation.domain.repositories.AuthRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class CategoriesUseCase @Inject constructor(
+    private val authRepository: AuthRepository,
+) {
+
+    suspend operator fun invoke(
+        role: String,
+        categoriesRequest: UserNameRequest,
+    ): Response<CategoriesResponse> =
+        authRepository.categories(role = role, categoriesRequest = categoriesRequest)
+
+}
