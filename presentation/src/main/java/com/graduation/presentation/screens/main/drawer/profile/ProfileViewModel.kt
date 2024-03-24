775d3c9b179a7645b1215c5ebd95fb5f528a3e94
Zeinab Abdien <105871085+ZeinabAbdien00@users.noreply.github.com>
2024-03-24 13:59:03 +0200

    Add profile screen

---
+++ presentation/src/main/java/com/graduation/presentation/screens/main/drawer/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.graduation.presentation.screens.main.drawer.profile
+
+import com.graduation.core.base.ui.BaseViewModel
+import javax.inject.Inject
+
+class ProfileViewModel @Inject constructor() : BaseViewModel() {
+}
